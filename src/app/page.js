d6e1b90183ad454a2bbd0fdde95b17401ee77090
MARIA EDUARDA SANTOS ALVES <maria.alves488>
2026-02-27 13:47:54 -0300

    Aula Css

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import styles from "./page.module.css"
 
-export default function Home() {
+function Home() {
   return (
     <div>
       <h1>React com Next Js </h1>
+      <h2>Aula introdução</h2>
+      <h3>Layout e estilização</h3>
+      <p>Nesta Aula iremos entender o uso do css global e local.</p>
     </div>
   );
 }
+export default Home;
+
