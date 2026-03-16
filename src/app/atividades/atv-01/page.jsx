a511ea46684a6a7e1c96f54722067e934fef844d
MARIA EDUARDA SANTOS ALVES <maria.alves488>
2026-03-16 15:23:19 -0300

    aula atividades

---
+++ src/app/atividades/atv-01/page.jsx
@@ -0,0 +1,28 @@
+
+import styles from "./page.module.css";
+function Atividades() {
+    return (
+       <> <div className={styles.containerh1}><h1> atividades</h1>
+      <h1>teste</h1>
+      <h2>teste</h2>
+      <h3>teste</h3>
+      </div>
+
+       <div className={styles.containerListas}>
+      
+
+        <div >
+            <div>
+                
+                <p> Componente básico para rederizar tela com React  </p>
+            </div>
+        </div>
+        </div>
+        </>
+    )
+
+}
+
+export default Atividades;
+
+
